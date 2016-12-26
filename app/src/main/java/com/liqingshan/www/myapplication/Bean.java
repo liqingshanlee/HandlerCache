3373620c2304d5c7cef33dccaec35293fe841c41
liqingshan <1182102704@qq.com>
2016-12-26 14:53:00 +0800

    fix

---
+++ app/src/main/java/com/liqingshan/www/myapplication/Bean.java
@@ -0,0 +1,27 @@
+package com.liqingshan.www.myapplication;
+
+/**
+ * 作者： 李青山
+ * 创建时间： 2016/12/26.
+ */
+
+public class Bean {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String name;
+    public String time;
+}
--- app/src/main/java/com/liqingshan/www/myapplication/MainActivity.java
+++ app/src/main/java/com/liqingshan/www/myapplication/MainActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mButton;
     private ProgressBar mProgressbar;
+    private TextView mTextView;
+    private Bean mBean=new Bean();
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mButton = (Button) findViewById(R.id.start);
         mProgressbar = (ProgressBar) findViewById(R.id.progress_bar);
+        mTextView=(TextView)findViewById(R.id.textview1);
         mButton.setOnClickListener(this);
     }
 
@@ -25,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.start:
+                i=0;
                 initProgressbar();
                 break;
         }
@@ -38,24 +44,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     Runnable update_thread = new Runnable() {
-        int i = 0;
-
         @Override
         public void run() {
             i += 10;
             Message message =mHandler.obtainMessage();
             message.arg1=i;
+            mBean.setName("名字是"+i);
+            mBean.setTime("时间是"+i);
+            message.obj=mBean;
             //延迟一个小时
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //把消息发送到消息队列中
+            //把消息添加到消息队列中
             mHandler.sendMessage(message);
             if(i==100){
                 //把线程从线程队列中移除
                 mHandler.removeCallbacks(update_thread);
+                return;
             }
         }
     };
@@ -65,6 +73,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void handleMessage(Message msg) {
             //显示进度条
             mProgressbar.setProgress(msg.arg1);
+            Bean mBean=(Bean) msg.obj;
+            mTextView.setText(mBean.getName()+mBean.getTime());
             //重新把进程加入到进程队列中
             mHandler.post(update_thread);
         }
